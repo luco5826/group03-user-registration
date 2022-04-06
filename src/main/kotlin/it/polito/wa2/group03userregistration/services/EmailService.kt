6e0067fa04ba2bfe461767d4b6d96aa6520c0952
Ale142 <ale.bacci142@gmail.com>
2022-04-06 18:43:31 +0200

    send mail functionality

--- src/main/kotlin/it/polito/wa2/group03userregistration/services/EmailService.kt
+++ src/main/kotlin/it/polito/wa2/group03userregistration/services/EmailService.kt
@@ -6,18 +6,36 @@ import it.polito.wa2.group03userregistration.entities.Activation
 import it.polito.wa2.group03userregistration.entities.User
 import it.polito.wa2.group03userregistration.repositories.ActivationRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.mail.MailException
+import org.springframework.mail.SimpleMailMessage
+import org.springframework.mail.javamail.JavaMailSender
 import org.springframework.stereotype.Service
+import java.text.SimpleDateFormat
+import java.util.*
 
 @Service
-class EmailService {
+class EmailService{
 
     private val ACTIVATIONCODE_LENGTH = 10
     private val charPool: List<Char> = ('a'..'z') + ('A'..'Z')+('0'..'9')
     @Autowired
     lateinit var activationRepository: ActivationRepository
-    fun insertActivation(user: User): ActivationDTO {
-        val savedEntity = activationRepository.save(Activation(user, generateActivationCode()))
-        return savedEntity.DTO()
+
+    @Autowired
+    lateinit var mailSender: JavaMailSender;
+
+    fun insertActivation(user: User): ActivationDTO? {
+        var savedEntity: Activation? = null
+
+        try {
+            savedEntity = activationRepository.save(Activation(user, generateActivationCode()))
+            sendMail(savedEntity.userActivation.email, savedEntity.userActivation.username,savedEntity.activationCode, savedEntity.expirationDate!!)
+        } catch (e: MailException) {
+            e.printStackTrace()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return savedEntity?.DTO()
     }
 
     fun generateActivationCode(): String {
@@ -25,6 +43,16 @@ class EmailService {
             .map { kotlin.random.Random.nextInt(0, charPool.size) }
             .map { charPool[it] }
             .joinToString("")
+    }
+
+    fun sendMail(toEmail: String, username: String, activationCode: String, expirationDate: Date){
+        val message = SimpleMailMessage()
+        message.setFrom("group03NML@gmail.com")
+        message.setTo(toEmail)
+        message.setText("Hello $username! This is your activation code $activationCode. Please use it before ${SimpleDateFormat("yyyy-mm-dd hh:mm:ss").format(expirationDate)}.\nHave a nice day!")
+        message.setSubject("Activation code")
+
+        mailSender.send(message)
     }
 
 }
