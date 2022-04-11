9f616be47843a4727a890ac6cc144ab509f8000c
fxbrit <fxbrit@tuta.io>
2022-04-11 20:09:43 +0200

    test user validity scenarios

--- src/test/kotlin/it/polito/wa2/group03userregistration/service/UserServiceTest.kt
+++ src/test/kotlin/it/polito/wa2/group03userregistration/service/UserServiceTest.kt
@@ -1,6 +1,7 @@
 package it.polito.wa2.group03userregistration.service
 
 import it.polito.wa2.group03userregistration.dtos.UserDTO
+import it.polito.wa2.group03userregistration.entities.User
 import it.polito.wa2.group03userregistration.enums.UserValidationStatus
 import it.polito.wa2.group03userregistration.services.UserService
 import org.junit.Ignore
@@ -36,6 +37,35 @@ class UserServiceTest {
 
     @Autowired
     lateinit var userService: UserService
+
+    @Test
+    fun testIsValidUser() {
+
+        val username = "user1"
+        val psw = "P4ssw0rd!"
+        val email = "user1@maildomain.invalid"
+
+        /** a valid user */
+        val validUser = User(username, psw, email)
+        Assertions.assertEquals(UserValidationStatus.VALID, userService.isValidUser(validUser))
+
+        /** user with blank username */
+        val blankUsername = User("", psw, email)
+        Assertions.assertEquals(UserValidationStatus.NO_USERNAME, userService.isValidUser(blankUsername))
+
+        /** user with blank email and broken email */
+        val blankEmail = User(username, psw, "")
+        val wrongEmail = User(username, psw, "wrong-looking-email")
+        Assertions.assertEquals(UserValidationStatus.NO_EMAIL, userService.isValidUser(blankEmail))
+        Assertions.assertEquals(UserValidationStatus.INVALID_EMAIL, userService.isValidUser(wrongEmail))
+
+        /** user with blank password and weak password */
+        val blankPassword = User(username, "", email)
+        val weakPassword = User(username, "ab12", email)
+        Assertions.assertEquals(UserValidationStatus.NO_PASSWORD, userService.isValidUser(blankPassword))
+        Assertions.assertEquals(UserValidationStatus.WEAK_PASSWORD, userService.isValidUser(weakPassword))
+
+    }
 
     @Ignore
     @Test
