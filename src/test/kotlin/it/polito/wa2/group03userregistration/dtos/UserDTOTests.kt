086fd67a4da39730bda153af7a562d33dcba14c2
fxbrit <fxbrit@tuta.io>
2022-04-11 11:16:36 +0200

    port UserDTOTests.kt to disposable containers setup

--- src/test/kotlin/it/polito/wa2/group03userregistration/dtos/UserDTOTests.kt
+++ src/test/kotlin/it/polito/wa2/group03userregistration/dtos/UserDTOTests.kt
@@ -1,12 +1,43 @@
 package it.polito.wa2.group03userregistration.dtos
 
 import it.polito.wa2.group03userregistration.entities.User
+import it.polito.wa2.group03userregistration.repositories.UserRepository
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.web.server.LocalServerPort
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
+import org.testcontainers.containers.PostgreSQLContainer
+import org.testcontainers.junit.jupiter.Container
+import org.testcontainers.junit.jupiter.Testcontainers
 
-@SpringBootTest
+@Testcontainers
+@SpringBootTest(webEnvironment=SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
 class UserDTOTests {
+
+    companion object {
+        @Container
+        val postgres = PostgreSQLContainer("postgres:latest")
+
+        @JvmStatic
+        @DynamicPropertySource
+        fun properties(registry: DynamicPropertyRegistry) {
+            registry.add("spring.datasource.url", postgres::getJdbcUrl)
+            registry.add("spring.datasource.username", postgres::getUsername)
+            registry.add("spring.datasource.password", postgres::getPassword)
+            registry.add("spring.jpa.hibernate.ddl-auto") {"create-drop"}
+        }
+    }
+
+    @LocalServerPort
+    protected var port: Int = 0
+
+    @Autowired
+    lateinit var userRepository: UserRepository
 
     @Test
     fun testConstructor() {
@@ -19,31 +50,34 @@ class UserDTOTests {
 
     @Test
     fun testToDTO() {
-        val user = User("user1", null, "user1@email.com")
-        val userDto = UserDTO(null, "user1", "user1@email.com", null)
-        Assertions.assertEquals(user.toUserDTO(), userDto)
+        val user = User("user1", "psw1", "user1@email.com")
+        val savedUser = userRepository.save(user)
+        val userDto = UserDTO(savedUser.id, "user1", "user1@email.com", "psw1")
+        Assertions.assertEquals(savedUser.toDTO(), userDto)
     }
 
     @Test
     fun testToDTOWrongUsername() {
-        val user = User("user1", null, "user1@email.com")
-        val userDto = UserDTO(null, "another_user", "user1@email.com", null)
-        Assertions.assertNotEquals(user.toUserDTO(), userDto)
+        val user = User("user1", "psw1", "user1@email.com")
+        val savedUser = userRepository.save(user)
+        val userDto = UserDTO(savedUser.id, "another_user", "user1@email.com", "psw1")
+        Assertions.assertNotEquals(savedUser.toDTO(), userDto)
     }
 
     @Test
     fun testToDTOWrongEmail() {
-        val user = User("user1", null, "user1@email.com")
-        val userDto = UserDTO(null, "user1", "another_user@email.com", null)
-        Assertions.assertNotEquals(user.toUserDTO(), userDto)
+        val user = User("user1", "psw1", "user1@email.com")
+        val savedUser = userRepository.save(user)
+        val userDto = UserDTO(savedUser.id, "user1", "another_user@email.com", "psw1")
+        Assertions.assertNotEquals(savedUser.toDTO(), userDto)
     }
 
     @Test
-    fun testToDTOWrongPassword() {
+    fun testToDTOChangePassword() {
         val user = User("user1", null, "user1@email.com")
-        Assertions.assertNull(user.toUserDTO().password)
-        user.password = "psw1"
-        Assertions.assertEquals(user.toUserDTO().password, "psw1")
+        Assertions.assertNull(user.toDTO().password)
+        user.password = "brand_new_psw"
+        Assertions.assertEquals(user.toDTO().password, "brand_new_psw")
     }
 
 }
